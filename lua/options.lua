e705883b77ce22c581fccd6ee8bb699b70449631
sagewe <sagewe@fastmail.com>
2025-01-15 11:28:39 +0800

    fix surround

--- lua/options.lua
+++ lua/options.lua
@@ -40,7 +40,7 @@ vim.opt.signcolumn = 'yes'
 vim.opt.updatetime = 250
 
 -- Decrease mapped sequence wait time
-vim.opt.timeoutlen = 300
+vim.opt.timeoutlen = 500
 
 -- Configure how new splits should be opened
 vim.opt.splitright = true
